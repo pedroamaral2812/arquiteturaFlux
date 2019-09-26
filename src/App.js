b64f99c9a8261b9bbf1a5eb02d4343ed5f232a8f
Pedro Amaral <pedropaulo@life.com.br>
2019-09-25 21:05:10 -0300

    Styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
-import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   )
 }
